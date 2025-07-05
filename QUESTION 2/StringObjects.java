cbc7ed4b233d7b41917386c66b60bdfdfdc5f4ad
Phillipsein <84603055+Phillipsein@users.noreply.github.com>
2025-07-05 13:33:06 +0300

    Create StringObjects.java
    2 String Objects in Java

---
+++ QUESTION 2/StringObjects.java
@@ -0,0 +1,4 @@
+1. // 1) Literal syntax: interned empty string from the pool
+2. String title = "";
+3. // 2) Constructor call: creates a new, distinct empty String object
+4. String author = new String();
